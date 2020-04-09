eb0cac80970febe7460b96dbe32cde4ecc9c4cb9
George-Stephen <yazz777stevie@gmail.com>
2020-04-09 17:55:44 +0300

    displaying repos

--- app/profile/profile.component.ts
+++ app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ServicesService} from './services.service';
+import {ReposEnquiryService} from '../repos-enquiry.service'
 import {User} from '../user'
 
 @Component({
@@ -9,13 +10,20 @@ import {User} from '../user'
 })
 export class ProfileComponent implements OnInit {
   profiles : any;
+  repos : any;
 
-  constructor(  private findprofile : ServicesService) { 
+  constructor(  private findprofile : ServicesService,private repos_get : ReposEnquiryService) { 
     this.findprofile.getprofile().subscribe(userprofile =>{
       console.log(userprofile)
       this.profiles = userprofile
 
     })
+    this.repos_get.getrepos().subscribe(repos =>{
+      
+      this.repos = repos;
+      console.log(this.repos);
+    })
+
   }
 
   ngOnInit(): void {
--- app/repos-enquiry.service.ts
+++ app/repos-enquiry.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,12 @@ export class ReposEnquiryService {
   private clientid: '728f208a302d5b900d25';
   private clientsecret : '6dcfb305cb5931322d8b9a372511abb7fa5924db';
 
-  constructor() { }
+  constructor( private http : HttpClient ) { 
+    console.log("It's Alive !");
+    this.Username = "George-Stephen";
+  }
+  getrepos(){
+    
+    return this.http.get("https://api.github.com/users/" + this.Username + "/repos?client_id =" + this.clientid + "&client_secret=" + this.clientsecret)
+  }
 }
